58ccc95f6cceea0a66c6930f817e44f74c48dfae
BILQIS HAFSAH <bilqis.hafsah.ababiel@mail.ugm.ac.id>
2023-03-25 05:46:04 +0700

    dashboard & admin view

---
+++ application/views/admin/_partials/side_nav.php
@@ -0,0 +1,9 @@
+<aside class="side-nav">
+	<div class="brand">
+		<h2>Toko HP</h2>
+	</div>
+	<nav>
+		<a href="<?= site_url('admin/dashboard') ?>">Overview</a>
+		<a href="<?= site_url('admin/product') ?>">Product</a>
+	</nav>
+</aside>
---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view('admin/_partials/head.php') ?>
+</head>
+
+<body>
+	<main class="main">
+		<?php $this->load->view('admin/_partials/side_nav.php') ?>
+		<div class="content">
+			<h1>Welcome Admin</h1>	
+		</div>
+	</main>
+</body>
+
+</html>
