e1a3c058533764901066fe9ba28886d674e00e5c
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2023-03-25 11:00:20 +0700

    Form validation and Re-populating

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,13 +3,40 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
-        $this->load->view('auth/login');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+
+        if ($this->form_validation->run('login') == false) {
+            $this->load->view('auth/login');
+        } else {
+            // TODO: login process dan masuk ke dashboard
+        }
     }
 
     public function register()
     {
-        $this->load->view('auth/register');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run('register') == false) {
+            $this->load->view('auth/register');
+        } else {
+            // TODO: register process dan redirect ke login
+        }
     }
 }
